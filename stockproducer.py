3b7432664827298fddbb72501e655c988806fcb6
Francesco <ftisiot@aiven.io>
2021-10-14 15:05:02 +0200

    changed weights

--- stockproducer.py
+++ stockproducer.py
@@ -7,7 +7,7 @@ StockNames = ['Deja Brew', 'Jurassic Pork', 'Lawn & Order', 'Pita Pan', 'Bread P
 StockCurrentValues = [10.0, 20.1, 20.2, 12.1, 25.1, 25.1, 27.5]
 StockUpProb = [0.5, 0.6, 0.7, 0.8, 0.9, 0.4, 0.3]
 ShuffleProb = 0.2
-ChangeAmount = 0.2
+ChangeAmount = 0.8
 
 class StockProvider(BaseProvider):
 
